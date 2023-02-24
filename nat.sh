86e6c0915d924f5c3484259ad8229cfd8d44798a
samaelleo <76465437+samaelleo@users.noreply.github.com>
2023-02-24 12:54:22 +0330

    Add files via upload

---
+++ nat.sh
@@ -0,0 +1,9 @@
+sudo iptables -t nat -D PREROUTING 1
+sudo iptables -t nat -D PREROUTING 1
+sudo iptables -t nat -D POSTROUTING 1
+sysctl net.ipv4.ip_forward=1
+iptables -t nat -A PREROUTING -p tcp --dport 22 -j DNAT --to-destination 62.60.160.12
+iptables -t nat -A PREROUTING -j DNAT --to-destination 173.232.146.42
+iptables -t nat -A POSTROUTING -j MASQUERADE
+sudo iptables -t nat -v -L -n --line-number
+
